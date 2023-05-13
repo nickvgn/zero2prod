1f78facc1f2a153fe165a9c468e08c4cd1f42d21
Nick Vegean <nick.vegean@involve.asia>
2023-05-13 18:13:32 +0800

    feat: postgres db configuration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    println!("Listening on {}", listener.local_addr().unwrap());
+    zero2prod::run(listener)?.await
 }
